3a65c890dc915780e5b41e8f77b2429aecb0be27
towa.sasaki <towa.sasaki@rakuten.com>
2018-05-24 04:04:55 +0000

    bootstrap commit

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -4,8 +4,7 @@
 
     <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
 
-    <p>タスク⇒{{ $task->content }}<br>ステータス⇒{{ $task->content }}</p>
-
+    <p>タスク⇒{{ $task->content}}<br>ステータス⇒{{ $task->status}}</p>
     {!! link_to_route('tasks.edit', 'このタスク編集', ['id' => $task->id]) !!}
 
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
